6418e14b8b19c3dfa16df99a6e008ce4277a20ab
JonasAtzenhofer <jomasatze@gmail.com>
2022-11-18 15:43:36 +0100

    push

--- A06/A05/script.js
+++ A06/A05/script.js
@@ -13,132 +13,143 @@ var ShoppingListL06;
         let item = await response.text();
         let data = JSON.parse(item);
         generateExistingItem(data);
-    }
-    function generateExistingItem(_data) {
-        let values = _data[1];
-        console.log(values[0].newItem);
-        let newItem = values[0].newItem;
-        let amount = values[0].amount;
-        let comment = values[0].comment;
-        let list = document.getElementById("list");
-        let newDiv = document.createElement("div");
-        let newInput = document.createElement("input");
-        let divItemData = document.createElement("div");
-        createInput(newInput, newDiv);
-        createDiv(newDiv);
-        createItemDiv(divItemData, newDiv);
-        addElement(divItemData);
-        addElement(divItemData, newItem.toString());
-        addElement(divItemData, amount.toString());
-        addElement(divItemData, comment.toString());
-        addElement(divItemData, dateWithoutTime);
-        addButton(newDiv, "edit");
-        addButton(newDiv, "delete");
-        list.appendChild(newDiv);
-    }
-    async function itemAdd() {
-        let formData = new FormData(document.querySelector("form"));
-        let newItem = formData.get("newItem");
-        let amount = formData.get("amount");
-        let comment = formData.get("comment");
-        let list = document.getElementById("list");
-        let newDiv = document.createElement("div");
-        let newInput = document.createElement("input");
-        let divItemData = document.createElement("div");
-        itemNumber++;
-        createInput(newInput, newDiv);
-        createDiv(newDiv);
-        createItemDiv(divItemData, newDiv);
-        addElement(divItemData);
-        addElement(divItemData, newItem.toString());
-        addElement(divItemData, amount.toString());
-        addElement(divItemData, comment.toString());
-        addElement(divItemData, dateWithoutTime);
-        addButton(newDiv, "edit");
-        addButton(newDiv, "delete");
-        list.appendChild(newDiv);
-        let query = new URLSearchParams(formData);
-        await fetch("index.html?" + query.toString());
-        console.log(query.toString());
-        alert("Item added");
-    }
-    function addElement(_parent, _content) {
-        let newItemField = document.createElement("p");
-        _parent.appendChild(newItemField);
-        newItemField.setAttribute("class", "ItemDataFont");
-        if (_content) {
-            newItemField.innerHTML = _content;
+        // create a async function to exchange the data with the server//
+        async function communicate(_url) {
+            let response = await fetch(_url);
+            let responseText = await response.text();
+            console.log(responseText);
+            let json = {};
+            for (let key of Object.keys(json)) {
+                let value = json[key];
+                console.log(key + ":" + value);
+            }
+            function generateExistingItem(_data) {
+                let values = _data[1];
+                console.log(values[0].newItem);
+                let newItem = values[0].newItem;
+                let amount = values[0].amount;
+                let comment = values[0].comment;
+                let list = document.getElementById("list");
+                let newDiv = document.createElement("div");
+                let newInput = document.createElement("input");
+                let divItemData = document.createElement("div");
+                createInput(newInput, newDiv);
+                createDiv(newDiv);
+                createItemDiv(divItemData, newDiv);
+                addElement(divItemData);
+                addElement(divItemData, newItem.toString());
+                addElement(divItemData, amount.toString());
+                addElement(divItemData, comment.toString());
+                addElement(divItemData, dateWithoutTime);
+                addButton(newDiv, "edit");
+                addButton(newDiv, "delete");
+                list.appendChild(newDiv);
+            }
+            async function itemAdd() {
+                let formData = new FormData(document.querySelector("form"));
+                let newItem = formData.get("newItem");
+                let amount = formData.get("amount");
+                let comment = formData.get("comment");
+                let list = document.getElementById("list");
+                let newDiv = document.createElement("div");
+                let newInput = document.createElement("input");
+                let divItemData = document.createElement("div");
+                itemNumber++;
+                createInput(newInput, newDiv);
+                createDiv(newDiv);
+                createItemDiv(divItemData, newDiv);
+                addElement(divItemData);
+                addElement(divItemData, newItem.toString());
+                addElement(divItemData, amount.toString());
+                addElement(divItemData, comment.toString());
+                addElement(divItemData, dateWithoutTime);
+                addButton(newDiv, "edit");
+                addButton(newDiv, "delete");
+                list.appendChild(newDiv);
+                let query = new URLSearchParams(formData);
+                await fetch("index.html?" + query.toString());
+                console.log(query.toString());
+                alert("Item added");
+            }
+            function addElement(_parent, _content) {
+                let newItemField = document.createElement("p");
+                _parent.appendChild(newItemField);
+                newItemField.setAttribute("class", "ItemDataFont");
+                if (_content) {
+                    newItemField.innerHTML = _content;
+                }
+            }
+            function addButton(_parent, _identify) {
+                let newButton = document.createElement("button");
+                _parent.appendChild(newButton);
+                newButton.setAttribute("class", _identify);
+                newButton.setAttribute("id", _identify + itemNumber);
+                newButton.setAttribute("type", "button");
+                newButton.innerHTML = _identify;
+                switch (_identify) {
+                    case "edit":
+                        newButton.addEventListener("click", editItem);
+                        break;
+                    case "delete":
+                        newButton.addEventListener("click", deleteItem);
+                    default:
+                        break;
+                }
+            }
+            function createDiv(_element) {
+                _element.setAttribute("class", "lister");
+                _element.setAttribute("id", "lister" + itemNumber);
+            }
+            function createInput(_element, _parent) {
+                _parent.appendChild(_element);
+                _element.setAttribute("class", "bought");
+                _element.setAttribute("id", "bought" + itemNumber);
+                _element.setAttribute("type", "checkbox");
+                _element.addEventListener("change", itemBought);
+            }
+            function createItemDiv(_element, _parent) {
+                _parent.appendChild(_element);
+                _element.setAttribute("class", "ItemData");
+                _element.setAttribute("id", "ItemData" + itemNumber);
+            }
+            function itemBought(_event) {
+                let trigger = _event.target.id;
+                let triggerNum = trigger.replace(/\D/g, "");
+                let identifyer = parseInt(triggerNum);
+                //to be continued
+            }
+            function editItem(_event) {
+                let trigger = _event.target.id;
+                let triggerNumber = trigger.replace(/\D/g, "");
+                let identifyer = parseInt(triggerNumber);
+                let list = document.getElementById("lister" + identifyer);
+                list.setAttribute("contenteditable", "true");
+                let saveItem = function (_event) {
+                    let trigger = _event.target.id;
+                    let triggerNumber = trigger.replace(/\D/g, "");
+                    let identifyer = parseInt(triggerNumber);
+                    let list = document.getElementById("lister" + identifyer);
+                    list.setAttribute("contenteditable", "false");
+                    let saveButton = document.getElementById("save" + identifyer);
+                    saveButton.innerHTML = "edit";
+                    saveButton.setAttribute("id", "edit" + identifyer);
+                    saveButton.addEventListener("click", editItem);
+                };
+                let saveButton = document.getElementById("edit" + identifyer);
+                saveButton.innerHTML = "save";
+                saveButton.setAttribute("id", "save" + identifyer);
+                saveButton.removeEventListener("click", editItem);
+                saveButton.addEventListener("click", saveItem);
+            }
+            function deleteItem(_event) {
+                let trigger = _event.target.id;
+                let triggerNum = trigger.replace(/\D/g, "");
+                let identifyer = parseInt(triggerNum);
+                let list = document.getElementById("list");
+                let remIt = document.getElementById("lister" + identifyer);
+                list.removeChild(remIt);
+            }
         }
-    }
-    function addButton(_parent, _identify) {
-        let newButton = document.createElement("button");
-        _parent.appendChild(newButton);
-        newButton.setAttribute("class", _identify);
-        newButton.setAttribute("id", _identify + itemNumber);
-        newButton.setAttribute("type", "button");
-        newButton.innerHTML = _identify;
-        switch (_identify) {
-            case "edit":
-                newButton.addEventListener("click", editItem);
-                break;
-            case "delete":
-                newButton.addEventListener("click", deleteItem);
-            default:
-                break;
-        }
-    }
-    function createDiv(_element) {
-        _element.setAttribute("class", "lister");
-        _element.setAttribute("id", "lister" + itemNumber);
-    }
-    function createInput(_element, _parent) {
-        _parent.appendChild(_element);
-        _element.setAttribute("class", "bought");
-        _element.setAttribute("id", "bought" + itemNumber);
-        _element.setAttribute("type", "checkbox");
-        _element.addEventListener("change", itemBought);
-    }
-    function createItemDiv(_element, _parent) {
-        _parent.appendChild(_element);
-        _element.setAttribute("class", "ItemData");
-        _element.setAttribute("id", "ItemData" + itemNumber);
-    }
-    function itemBought(_event) {
-        let trigger = _event.target.id;
-        let triggerNum = trigger.replace(/\D/g, "");
-        let identifyer = parseInt(triggerNum);
-        //to be continued
-    }
-    function editItem(_event) {
-        let trigger = _event.target.id;
-        let triggerNumber = trigger.replace(/\D/g, "");
-        let identifyer = parseInt(triggerNumber);
-        let list = document.getElementById("lister" + identifyer);
-        list.setAttribute("contenteditable", "true");
-        let saveItem = function (_event) {
-            let trigger = _event.target.id;
-            let triggerNumber = trigger.replace(/\D/g, "");
-            let identifyer = parseInt(triggerNumber);
-            let list = document.getElementById("lister" + identifyer);
-            list.setAttribute("contenteditable", "false");
-            let saveButton = document.getElementById("save" + identifyer);
-            saveButton.innerHTML = "edit";
-            saveButton.setAttribute("id", "edit" + identifyer);
-            saveButton.addEventListener("click", editItem);
-        };
-        let saveButton = document.getElementById("edit" + identifyer);
-        saveButton.innerHTML = "save";
-        saveButton.setAttribute("id", "save" + identifyer);
-        saveButton.removeEventListener("click", editItem);
-        saveButton.addEventListener("click", saveItem);
-    }
-    function deleteItem(_event) {
-        let trigger = _event.target.id;
-        let triggerNum = trigger.replace(/\D/g, "");
-        let identifyer = parseInt(triggerNum);
-        let list = document.getElementById("list");
-        let remIt = document.getElementById("lister" + identifyer);
-        list.removeChild(remIt);
     }
 })(ShoppingListL06 || (ShoppingListL06 = {}));
 //# sourceMappingURL=script.js.map
--- A06/A05/script.ts
+++ A06/A05/script.ts
@@ -27,6 +27,12 @@ namespace ShoppingListL06 {
         [1]: ItemAdded[];
     }
 
+    interface FormDataJson {
+        [key: string]: FormDataEntryValue | FormDataEntryValue[];
+    }
+
+
+
 
 
 
@@ -42,190 +48,205 @@ namespace ShoppingListL06 {
         generateExistingItem(data);
 
 
+        // create a async function to exchange the data with the server//
+        async function communicate(_url: RequestInfo): Promise<void> {
+            let response: Response = await fetch(_url);
+            let responseText: string = await response.text();
+            console.log(responseText);
+
+            let json: FormDataJson = {};
+
+            for (let key of Object.keys(json)) {
+                let value: FormDataEntryValue = json[key];
+                console.log(key + ":" + value);
 
 
 
 
-    }
-
-    function generateExistingItem(_data: Data): void {
-        let values: ItemAdded[] = _data[1];
-        console.log(values[0].newItem);
-
-        let newItem: string = values[0].newItem;
-        let amount: number = values[0].amount;
-        let comment: string = values[0].comment;
-        let list: HTMLElement = document.getElementById("list");
-        let newDiv: HTMLDivElement = document.createElement("div");
-        let newInput: HTMLInputElement = document.createElement("input");
-        let divItemData: HTMLDivElement = document.createElement("div");
-
-        createInput(newInput, newDiv);
-
-        createDiv(newDiv);
-
-        createItemDiv(divItemData, newDiv);
-
-        addElement(divItemData);
-
-        addElement(divItemData, newItem.toString());
-
-        addElement(divItemData, amount.toString());
-
-        addElement(divItemData, comment.toString());
-
-        addElement(divItemData, dateWithoutTime);
-
-        addButton(newDiv, "edit");
-
-        addButton(newDiv, "delete");
-
-        list.appendChild(newDiv);
 
 
-    }
-
-    async function itemAdd(): Promise<void> {
-        let formData: FormData = new FormData(document.querySelector("form"));
-        let newItem: FormDataEntryValue = formData.get("newItem");
-        let amount: FormDataEntryValue = formData.get("amount");
-        let comment: FormDataEntryValue = formData.get("comment");
-        let list: HTMLElement = document.getElementById("list");
-        let newDiv: HTMLDivElement = document.createElement("div");
-        let newInput: HTMLInputElement = document.createElement("input");
-        let divItemData: HTMLDivElement = document.createElement("div");
-        itemNumber++;
-
-        createInput(newInput, newDiv);
-
-        createDiv(newDiv);
-
-        createItemDiv(divItemData, newDiv);
-
-        addElement(divItemData);
-
-        addElement(divItemData, newItem.toString());
-
-        addElement(divItemData, amount.toString());
-
-        addElement(divItemData, comment.toString());
-
-        addElement(divItemData, dateWithoutTime);
-
-        addButton(newDiv, "edit");
-
-        addButton(newDiv, "delete");
-
-        list.appendChild(newDiv);
 
 
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        await fetch("index.html?" + query.toString());
-        console.log(query.toString());
-        alert("Item added");
+            }
 
-    }
+            function generateExistingItem(_data: Data): void {
+                let values: ItemAdded[] = _data[1];
+                console.log(values[0].newItem);
 
-    function addElement(_parent: HTMLElement, _content?: string): void {
-        let newItemField: HTMLElement = document.createElement("p");
-        _parent.appendChild(newItemField);
-        newItemField.setAttribute("class", "ItemDataFont");
-        if (_content) {
-            newItemField.innerHTML = (_content as string);
+                let newItem: string = values[0].newItem;
+                let amount: number = values[0].amount;
+                let comment: string = values[0].comment;
+                let list: HTMLElement = document.getElementById("list");
+                let newDiv: HTMLDivElement = document.createElement("div");
+                let newInput: HTMLInputElement = document.createElement("input");
+                let divItemData: HTMLDivElement = document.createElement("div");
+
+                createInput(newInput, newDiv);
+
+                createDiv(newDiv);
+
+                createItemDiv(divItemData, newDiv);
+
+                addElement(divItemData);
+
+                addElement(divItemData, newItem.toString());
+
+                addElement(divItemData, amount.toString());
+
+                addElement(divItemData, comment.toString());
+
+                addElement(divItemData, dateWithoutTime);
+
+                addButton(newDiv, "edit");
+
+                addButton(newDiv, "delete");
+
+                list.appendChild(newDiv);
+
+
+            }
+
+            async function itemAdd(): Promise<void> {
+                let formData: FormData = new FormData(document.querySelector("form"));
+                let newItem: FormDataEntryValue = formData.get("newItem");
+                let amount: FormDataEntryValue = formData.get("amount");
+                let comment: FormDataEntryValue = formData.get("comment");
+                let list: HTMLElement = document.getElementById("list");
+                let newDiv: HTMLDivElement = document.createElement("div");
+                let newInput: HTMLInputElement = document.createElement("input");
+                let divItemData: HTMLDivElement = document.createElement("div");
+                itemNumber++;
+
+                createInput(newInput, newDiv);
+
+                createDiv(newDiv);
+
+                createItemDiv(divItemData, newDiv);
+
+                addElement(divItemData);
+
+                addElement(divItemData, newItem.toString());
+
+                addElement(divItemData, amount.toString());
+
+                addElement(divItemData, comment.toString());
+
+                addElement(divItemData, dateWithoutTime);
+
+                addButton(newDiv, "edit");
+
+                addButton(newDiv, "delete");
+
+                list.appendChild(newDiv);
+
+
+                let query: URLSearchParams = new URLSearchParams(<any>formData);
+                await fetch("index.html?" + query.toString());
+                console.log(query.toString());
+                alert("Item added");
+
+            }
+
+            function addElement(_parent: HTMLElement, _content?: string): void {
+                let newItemField: HTMLElement = document.createElement("p");
+                _parent.appendChild(newItemField);
+                newItemField.setAttribute("class", "ItemDataFont");
+                if (_content) {
+                    newItemField.innerHTML = (_content as string);
+                }
+
+            }
+
+            function addButton(_parent: HTMLElement, _identify: string): void {
+                let newButton: HTMLElement = document.createElement("button");
+                _parent.appendChild(newButton);
+                newButton.setAttribute("class", _identify);
+                newButton.setAttribute("id", _identify + itemNumber);
+                newButton.setAttribute("type", "button");
+                newButton.innerHTML = _identify;
+                switch (_identify) {
+                    case "edit":
+                        newButton.addEventListener("click", editItem);
+                        break;
+                    case "delete":
+                        newButton.addEventListener("click", deleteItem);
+                    default:
+                        break;
+                }
+            }
+
+            function createDiv(_element: HTMLElement): void {
+                _element.setAttribute("class", "lister");
+                _element.setAttribute("id", "lister" + itemNumber);
+            }
+
+            function createInput(_element: HTMLElement, _parent: HTMLElement): void {
+                _parent.appendChild(_element);
+                _element.setAttribute("class", "bought");
+                _element.setAttribute("id", "bought" + itemNumber);
+                _element.setAttribute("type", "checkbox");
+                _element.addEventListener("change", itemBought);
+            }
+
+            function createItemDiv(_element: HTMLElement, _parent: HTMLElement): void {
+                _parent.appendChild(_element);
+                _element.setAttribute("class", "ItemData");
+                _element.setAttribute("id", "ItemData" + itemNumber);
+            }
+
+            function itemBought(_event: Event): void {
+                let trigger: string = (_event.target as HTMLInputElement).id;
+                let triggerNum: string = trigger.replace(/\D/g, "");
+                let identifyer: number = parseInt(triggerNum);
+                //to be continued
+            }
+
+            function editItem(_event: Event): void {
+                let trigger: string = (_event.target as HTMLButtonElement).id;
+                let triggerNumber: string = trigger.replace(/\D/g, "");
+                let identifyer: number = parseInt(triggerNumber);
+                let list: HTMLElement = document.getElementById("lister" + identifyer);
+                list.setAttribute("contenteditable", "true");
+                let saveItem = function (_event: Event): void | null {
+                    let trigger: string = (_event.target as HTMLButtonElement).id;
+                    let triggerNumber: string = trigger.replace(/\D/g, "");
+                    let identifyer: number = parseInt(triggerNumber);
+                    let list: HTMLElement = document.getElementById("lister" + identifyer);
+                    list.setAttribute("contenteditable", "false");
+                    let saveButton: HTMLElement = document.getElementById("save" + identifyer);
+                    saveButton.innerHTML = "edit";
+                    saveButton.setAttribute("id", "edit" + identifyer);
+                    saveButton.addEventListener("click", editItem);
+                };
+
+
+
+                let saveButton: HTMLElement = document.getElementById("edit" + identifyer);
+                saveButton.innerHTML = "save";
+                saveButton.setAttribute("id", "save" + identifyer);
+                saveButton.removeEventListener("click", editItem);
+                saveButton.addEventListener("click", saveItem);
+
+
+
+
+
+
+
+
+
+            }
+
+
+            function deleteItem(_event: Event): void {
+                let trigger: string = (_event.target as HTMLButtonElement).id;
+                let triggerNum: string = trigger.replace(/\D/g, "");
+                let identifyer: number = parseInt(triggerNum);
+
+                let list: HTMLElement = document.getElementById("list");
+                let remIt: HTMLElement = document.getElementById("lister" + identifyer);
+                list.removeChild(remIt);
+            }
+
+
         }
-
-    }
-
-    function addButton(_parent: HTMLElement, _identify: string): void {
-        let newButton: HTMLElement = document.createElement("button");
-        _parent.appendChild(newButton);
-        newButton.setAttribute("class", _identify);
-        newButton.setAttribute("id", _identify + itemNumber);
-        newButton.setAttribute("type", "button");
-        newButton.innerHTML = _identify;
-        switch (_identify) {
-            case "edit":
-                newButton.addEventListener("click", editItem);
-                break;
-            case "delete":
-                newButton.addEventListener("click", deleteItem);
-            default:
-                break;
-        }
-    }
-
-    function createDiv(_element: HTMLElement): void {
-        _element.setAttribute("class", "lister");
-        _element.setAttribute("id", "lister" + itemNumber);
-    }
-
-    function createInput(_element: HTMLElement, _parent: HTMLElement): void {
-        _parent.appendChild(_element);
-        _element.setAttribute("class", "bought");
-        _element.setAttribute("id", "bought" + itemNumber);
-        _element.setAttribute("type", "checkbox");
-        _element.addEventListener("change", itemBought);
-    }
-
-    function createItemDiv(_element: HTMLElement, _parent: HTMLElement): void {
-        _parent.appendChild(_element);
-        _element.setAttribute("class", "ItemData");
-        _element.setAttribute("id", "ItemData" + itemNumber);
-    }
-
-    function itemBought(_event: Event): void {
-        let trigger: string = (_event.target as HTMLInputElement).id;
-        let triggerNum: string = trigger.replace(/\D/g, "");
-        let identifyer: number = parseInt(triggerNum);
-        //to be continued
-    }
-
-    function editItem(_event: Event): void {
-        let trigger: string = (_event.target as HTMLButtonElement).id;
-        let triggerNumber: string = trigger.replace(/\D/g, "");
-        let identifyer: number = parseInt(triggerNumber);
-        let list: HTMLElement = document.getElementById("lister" + identifyer);
-        list.setAttribute("contenteditable", "true");
-        let saveItem = function (_event: Event): void | null {
-            let trigger: string = (_event.target as HTMLButtonElement).id;
-            let triggerNumber: string = trigger.replace(/\D/g, "");
-            let identifyer: number = parseInt(triggerNumber);
-            let list: HTMLElement = document.getElementById("lister" + identifyer);
-            list.setAttribute("contenteditable", "false");
-            let saveButton: HTMLElement = document.getElementById("save" + identifyer);
-            saveButton.innerHTML = "edit";
-            saveButton.setAttribute("id", "edit" + identifyer);
-            saveButton.addEventListener("click", editItem);
-        };
-        
-
-
-        let saveButton: HTMLElement = document.getElementById("edit" + identifyer);
-        saveButton.innerHTML = "save";
-        saveButton.setAttribute("id", "save" + identifyer);
-        saveButton.removeEventListener("click", editItem);
-        saveButton.addEventListener("click", saveItem);
-
-
-
-
-
-
-
-
-
-    }
-
-
-    function deleteItem(_event: Event): void {
-        let trigger: string = (_event.target as HTMLButtonElement).id;
-        let triggerNum: string = trigger.replace(/\D/g, "");
-        let identifyer: number = parseInt(triggerNum);
-
-        let list: HTMLElement = document.getElementById("list");
-        let remIt: HTMLElement = document.getElementById("lister" + identifyer);
-        list.removeChild(remIt);
-    }
-
-
-}
