b54198c481bce29c8383405509093072b00773b7
JonasAtzenhofer <jomasatze@gmail.com>
2022-12-13 13:07:28 +0100

    push

--- A09.2/canvas.ts
+++ A09.2/canvas.ts
@@ -9,7 +9,7 @@
 
 
 
-namespace A08 {
+namespace A092 {
     window.addEventListener("load", handleLoad);
     interface Vector {
         x: number;
@@ -189,16 +189,36 @@ namespace A08 {
         function drawSnowflakes(_position: Vector, _size: Vector): void {
             console.log("Snowflakes", _position, _size);
 
+            
 
-            for (let i: number = 0; i < 100; i++) {
-                let x: number = Math.random() * crc2.canvas.width;
-                let y: number = Math.random() * crc2.canvas.height;
-                let radiusSnowflake: number = Math.random() * 3 + 1;
-                crc2.beginPath();
-                crc2.arc(x, y, radiusSnowflake, 0, 2 * Math.PI);
-                crc2.fillStyle = "white";
-                crc2.fill();
+            let nParticles: number = 20;
+            let radiusParticle: number = 50;
+            let particle: Path2D = new Path2D();
+            let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+
+            particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+            gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+            gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+
+            crc2.save();
+            crc2.translate(_position.x, _position.y);
+            crc2.fillStyle = gradient;
+
+            for (let drawn: number = 0; drawn < nParticles; drawn++) {
+                crc2.save();
+                let x: number = (Math.random() - 0.5) * _size.x;
+                let y: number = - (Math.random() * _size.y);
+                crc2.translate(x, y);
+                crc2.fill(particle);
+                crc2.restore();
             }
+
+            crc2.restore();
+
+
+
+            
+
         }
 
 
